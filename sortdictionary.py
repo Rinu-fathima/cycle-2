8fee04a617132f6a59f01462860ad6010757dd8a
Rinu-fathima <24mp2291@rit.ac.in>
2024-11-29 14:07:49 +0530

    Create sortdictionary.py

---
+++ sortdictionary.py
@@ -0,0 +1,6 @@
+person1={'name1':'shamja','name2':'rinu','name3':'appu'}
+print(f"Ascending sort of values:{sorted(person1.values())}")
+print(f"Descending sort of values:{sorted(person1.values(),reverse=True)}")
+print(f"Ascending sort of keys:{sorted(person1.keys())} ")
+print(f"Descending sort of keys:{sorted(person1.keys(),reverse=True)}")
+
