174c7e1cdac9e5a9038857586bd2c6672869ff1e
Rinu-fathima <24mp2291@rit.ac.in>
2024-11-29 13:56:02 +0530

    Create charfrequency.py

---
+++ charfrequency.py
@@ -0,0 +1,13 @@
+str=input("Enter a string:")
+str2=str.lower()
+count={}
+for char in str2:
+        if char in count:
+                count[char]+=1
+        else:
+                count[char]=1
+print(count)
+
+
+
+
